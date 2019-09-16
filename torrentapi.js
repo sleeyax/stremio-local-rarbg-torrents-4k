34db9c27fc38197f7b413f1794cddca1edf48647
Sleeyax <yourd3veloper@gmail.com>
2019-09-16 19:39:32 +0200

    Move to function, promisify & use persistent storage

--- torrentapi.js
+++ torrentapi.js
@@ -1,28 +1,10 @@
 const request = require('request');
 const { config, persist } = require('internal');
 
-let token = persist.token || ''
-
-request({
-    uri: 'https://torrentapi.org/pubapi_v2.php',
-    qs: { get_token: 'get_token', app_id: config.appId },
-    json: true,
-    headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0'
-    }
-}, (error, response, body) => {
-    if ((body || {}).token) {
-        console.log('RARBG 4k: Successfully fetched API token')
-        token = body.token
-    } else
-        console.log('RARBG 4k: ' + ((error || {}).message || 'Unknown error while fetching API token'))
-});
-
-
 class TorrentApi {
-    queryAPI(mode, params = {}, format = 'json_extended') {
+    async queryAPI(mode, params = {}, format = 'json_extended') {
         params.app_id = config.appId;
-        params.token = token;
+        params.token = persist.getItem('token') || await this.getToken(config.appId);
         params.sort = config.sortingMethod;
         params.min_seeders = config.minSeeders;
         params.mode = mode;
@@ -42,6 +24,24 @@ class TorrentApi {
             }, (error, response, body) => {
                 if (error) reject(error);
                 resolve(body);
+            });
+        });
+    }
+
+    getToken(appId) {
+        return new Promise((resolve, reject) => {
+            request({
+                uri: 'https://torrentapi.org/pubapi_v2.php',
+                qs: { get_token: 'get_token', app_id: appId },
+                json: true,
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0'
+                }
+            }, (error, response, body) => {
+                if (error) reject(error);
+                if (!body.token) reject('rarbg-4k: token not found in response');
+                persist.setItem('token', body.token);
+                resolve(body.token);
             });
         });
     }
