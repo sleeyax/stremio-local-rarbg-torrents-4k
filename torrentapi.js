adefd7ffb8dd96a28e6eed237c0a274bf08395a9
machaboo <52109294+machaboo@users.noreply.github.com>
2019-09-17 19:57:35 +0300

    Remove logs

--- torrentapi.js
+++ torrentapi.js
@@ -25,7 +25,6 @@ class TorrentApi {
                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0'
                 }
             }, (error, response, body) => {
-                console.log(response.statusCode)
                 if (error) reject(error);
                 resolve(body);
             });
